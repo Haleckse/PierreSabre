caee692aea8fbeb0e2c99a562c79ec4369d1e67d
Alex Tsan <tsl4297a@ensfsi.onmicrosoft.com>
2023-10-24 19:41:18 +0200

    Creation de la classe Humain

---
+++ src/histoire/HistoireTP4.java
@@ -0,0 +1,17 @@
+package histoire;
+import personnages.Humain;
+
+public class HistoireTP4 {
+
+	public static void main(String[] args) {
+		Humain Prof = new Humain("Prof", 50, "Kombucha");
+		Prof.direBonjour();
+		Prof.acheter("boisson", 12); 
+		Prof.boire();
+		Prof.acheter("jeu", 2);
+		Prof.acheter("kimono", 50); 
+		
+
+	}
+
+}
---
+++ src/personnages/Humain.java
@@ -0,0 +1,57 @@
+package personnages;
+
+public class Humain {
+	private String nom; 
+	private int argent; 
+	private String boissonFavorite; 
+	
+	public Humain(String nom, int argent, String boissonFavorite) {
+		this.nom = nom; 
+		this.argent = argent; 
+		this.boissonFavorite = boissonFavorite; 
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public int getArgent() {
+		return argent;
+	}
+	
+	private void parler(String texte) {
+		System.out.println("(" + nom + ")- " + texte);
+	}
+	
+	public void direBonjour() {
+		parler("Bonjour, je m'appelle " + getNom() + " et j'aime boire du " + boissonFavorite); 
+	}
+	
+	public void boire() {
+		parler("Mmmm, un bon verre de " + boissonFavorite + " GLOUPS !!"); 
+	}
+	
+	public void gagnerArgent(int gain) {
+		argent = argent + gain;  
+	}
+	
+	public void perdreArgent(int perte) {
+		argent = argent - perte; 
+	}
+	
+	public void acheter(String bien, int prix) {
+		if (argent >= prix) {
+			parler("J'ai " + getArgent() + " sous en poche. Je vais pouvoir m'offrir une " + bien + "à " + prix + " sous.");
+			perdreArgent(prix); 
+		}
+		else { 
+			parler("Je n ai plus que " + getArgent() + " sous en poche. Je ne peux même pas m'offrir un " + bien + " à " + prix + " sous."); 
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
