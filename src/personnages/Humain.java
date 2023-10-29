28ca0babbf198a73b6ee5b0d0a36583b7ab354d7
Haleckse <haleckse@gmail.com>
2023-10-29 15:40:31 +0100

    les humains ont des connaissances

--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -5,7 +5,7 @@ public class Humain {
 	private String nom; 
 	protected int argent; 
 	private String boissonFavorite; 
-	private int nombreConnaissanceMax = 3;
+	private int nombreConnaissanceMax = 30;
 	private Humain [] connaissance = new Humain[nombreConnaissanceMax]; 
 	public int nombreConnaissance = 0; 
 	
@@ -45,11 +45,11 @@ public class Humain {
 	
 	public void acheter(String bien, int prix) {
 		if (argent >= prix) {
-			parler("J'ai " + getArgent() + " sous en poche. Je vais pouvoir m'offrir une " + bien + "� " + prix + " sous.");
+			parler("J'ai " + getArgent() + " sous en poche. Je vais pouvoir m'offrir une " + bien + " a " + prix + " sous.");
 			perdreArgent(prix); 
 		}
 		else { 
-			parler("Je n ai plus que " + getArgent() + " sous en poche. Je ne peux m�me pas m'offrir un " + bien + " � " + prix + " sous."); 
+			parler("Je n ai plus que " + getArgent() + " sous en poche. Je ne peux meme pas m'offrir un " + bien + " a " + prix + " sous."); 
 		}
 	}
 	
@@ -60,10 +60,10 @@ public class Humain {
 			nombreConnaissance = nombreConnaissance + 1;
 		}
 		else {
-			for (int i = 1; i <= nombreConnaissance; i++) {
+			for (int i = 1; i < nombreConnaissance; i++) {
 				connaissance[i-1] = connaissance[i]; 
-				connaissance[nombreConnaissance-2] = autreHumain;
 			}
+			connaissance[nombreConnaissance-1] = autreHumain;
 		}
 	}
 	
